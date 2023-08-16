648477fbdb8e53960cde22aa9857d6ff52be9bc8
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-15 22:43:24 -0700

    5-unique_id.sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the database name is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <database_name>"
+  exit 1
+fi
+
+# Database name provided as an argument
+db_name="$1"
+
+# SQL script content
+sql_script="CREATE TABLE IF NOT EXISTS ${db_name}.unique_id (
+  id INT NOT NULL DEFAULT 1,
+  name VARCHAR(256),
+  UNIQUE (id)
+);"
+
+# Run the SQL script using the mysql command
+mysql -u root -p -e "$sql_script"
