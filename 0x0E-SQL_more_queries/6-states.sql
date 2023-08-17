4083b73a90112cae4b6131ef9a97aeb2b48d70f0
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-17 00:22:41 -0700

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,23 +1,6 @@
-#!/bin/bash
-
-# Check if the database name is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-
-# Database name provided as an argument
-db_name="$1"
-
-# SQL script content
-sql_script="CREATE DATABASE IF NOT EXISTS ${db_name};
-USE ${db_name};
-CREATE TABLE IF NOT EXISTS states (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE (id)
-);"
-
-# Run the SQL script using the mysql command
-mysql -u root -p -e "$sql_script"
+-- creates the database hbtn_0d_usa and the table states on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	);
