177eef32a3d548de2331ddf408b51d8d0d0ce282
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-17 00:13:27 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,11 +1,4 @@
--- Connect to the MySQL server
-mysql -u root -p
-
--- Run the following SQL commands in the MySQL shell
-
--- Create the user user_0d_1 with all privileges and set the password
+-- creates the MySQL server user user_0d_1.
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Exit the MySQL shell
-exit
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
