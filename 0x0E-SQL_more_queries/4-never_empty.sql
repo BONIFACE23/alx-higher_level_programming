2e0ba5532e06ffdcd44eaca3712590fe59eeb524
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-17 00:19:05 -0700

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,19 +1,5 @@
-#!/bin/bash
-
-# Check if the database name is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-
-# Database name provided as an argument
-db_name="$1"
-
-# SQL script content
-sql_script="CREATE TABLE IF NOT EXISTS ${db_name}.id_not_null (
-  id INT NOT NULL DEFAULT 1,
-  name VARCHAR(256)
-);"
-
-# Run the SQL script using the mysql command
-mysql -u root -p -e "$sql_script"
+-- creates the table id_not_null on your MySQL server.
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT NOT NULL,
+	name VARCHAR(256)
+	);
