4ba85a2e29e3f9554064eed933445562af479d9e
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-17 00:15:30 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,14 +1,5 @@
--- Connect to the MySQL server
-mysql -u root -p
-
--- Run the following SQL commands in the MySQL shell
-
--- Create the database hbtn_0d_2 if it doesn't exist
+-- creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user user_0d_2 if it doesn't exist and grant SELECT privilege
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Exit the MySQL shell
-exit
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
