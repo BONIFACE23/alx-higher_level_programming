7e33ca5fb6a4685b95c68a66a1b57482b5a51dc9
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-16 23:23:20 -0700

    1-create_database_if_missing.sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,33 @@
+import mysql.connector
+
+# Replace these values with your MySQL server credentials
+host = "localhost"
+user = "your_username"
+password = "your_password"
+database_name = "hbtn_0c_0"
+
+try:
+    # Connect to the MySQL server
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = connection.cursor()
+
+    # Create the database if it doesn't exist
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+
+    print(f"Database '{database_name}' created successfully.")
+
+except mysql.connector.Error as err:
+    print("Error:", err)
+
+finally:
+    # Close the cursor and connection
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
