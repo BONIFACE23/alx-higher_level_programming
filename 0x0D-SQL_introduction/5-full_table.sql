8e40588d6735cc6ecc4650524a488a719bec9ecf
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-16 23:44:51 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
-USE your_database_name;
-
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+-- prints the full description of the table first_table from the database hbtn_0c_0
+SHOW CREATE TABLE first_table;
