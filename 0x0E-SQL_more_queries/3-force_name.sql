8060dfc49504697bf0d270b643c298874b0fedf2
BONIFACE23 <irunguboniface413@gmail.com>
2023-08-17 00:17:29 -0700

    3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,19 +1,5 @@
-#!/bin/bash
-
-# Check if the database name is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-
-# Database name provided as an argument
-db_name="$1"
-
-# SQL script content
-sql_script="CREATE TABLE IF NOT EXISTS ${db_name}.force_name (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL
-);"
-
-# Run the SQL script using the mysql command
-mysql -u root -p -e "$sql_script"
+-- creates the table force_name on your MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+	);
