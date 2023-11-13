a41d215fa12716cba056e64d44741d8fab072262
BONIFACE23 <irunguboniface413@gmail.com>
2023-11-13 03:13:40 -0800

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const length = process.argv.length;
+
+if (length <= 3) {
+  console.log(0);
+} else {
+  const myList = [];
+  for (let i = 0; i < length - 2; i++) {
+    myList[i] = parseInt(process.argv[i + 2]);
+  }
+  myList.sort((a, b) => a - b);
+  myList.reverse();
+  console.log(myList[1]);
+}
