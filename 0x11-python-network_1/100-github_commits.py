43182d1cc7466641d6c07dc5ca5893685083e149
BONIFACE23 <irunguboniface413@gmail.com>
2023-12-01 08:13:22 -0800

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Model to takes 2 arguments in order to list 10 commits"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+            .format(sys.argv[2], sys.argv[1])
+    resp = requests.get(url)
+    try:
+        js_r = resp.json()
+        for com in js_r:
+            print("{}: {}".format(com.get('sha'), com.get('commit')
+                                  .get('author').get('name')))
+    except Exception:
+        pass
