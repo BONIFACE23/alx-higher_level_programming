dda77da028d8f79698660c24702611a13278e0cf
BONIFACE23 <irunguboniface413@gmail.com>
2024-01-04 03:52:27 -0800

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Writes a string to a file.
+
+const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
+try {
+	fs.writeFileSync(filename, content, 'utf-8');
+} catch (error) {
+	console.log(error);
+}
