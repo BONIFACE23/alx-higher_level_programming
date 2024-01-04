a0c71cbb23364f23d271fcb443d4bcccb1186868
BONIFACE23 <irunguboniface413@gmail.com>
2024-01-04 04:05:15 -0800

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Display the status code of a GET request.
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (_error, response) {
+	const todo = JSON.parse(response.body);
+	const complete = [];
+	todo.forEach((s) => s.completed === true ? complete.push(s) : 0);
+	const dict1 = {};
+	let i = 1;
+	while (true) {
+		const x = [];
+		complete.forEach((s) => s.userId === i ? x.push(s) : 0);
+		if (x.length !== 0) {
+			dict1[i] = x.length;
+		} else {
+			break;
+		}
+		i++;
+	}
+	console.log(dict1);
+});
